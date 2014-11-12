c0d55ae1c2340ec7ceb35ef385cfa42fe881905b
Karl Tayfer <daPhyre.x@gmail.com>
2014-11-12 00:31:07 -0600

    JSCompiler2 Beta

--- README.md
+++ README.md
@@ -1,15 +1,38 @@
-# JSCompiler for [Brackets](https://github.com/adobe/brackets)
+# JSCompiler2 for [Brackets](https://github.com/adobe/brackets)
 
-JSCompiler is a tool for [Brackets](https://github.com/adobe/brackets) that allows you to compress and mangle your JavaScript code into one minified file, powered by [UglifyJS2](https://github.com/mishoo/UglifyJS2)
+JSCompiler2 is a tool for [Brackets](https://github.com/adobe/brackets) that allows you to compress and mangle your JavaScript code into one minified file, powered by [UglifyJS2](https://github.com/mishoo/UglifyJS2)
 
 ## Usage
 
-Simply press the "Compress JavaScript" button on the sidebar, or go to File > Compress JavaScript, and your code will be compressed into a `{filename}.min.js` file.
+Simply press the "Compress JavaScript" button on the sidebar, or go to `File > Compress JavaScript`, and your code will be compressed into a `{filename}.min.js` file.
 
-## Another JavaScript Compresser for Brackets?
+### Advanced options
 
-I'm aware that this is not the first compress tool for JavaScript in Brackets, but those I've tried, did require previous configuration to the project, or send the compression task to a website, making it poorly optimized and potentially unreliable.
+You can go to `File > Compress JavaScript: Options` to open a JSON file with the compiler options for the currect directory. There you can customize next values:
 
-With this in mind, I decided to make my own version, that was simple, fast and reliable for this task, resulting on the extension that is now here.
+- **Project**: The name of the current project.
+- **Inputs**: An array of javascript files to be compressed into the minified file.
+- **Output**: The name of the resulting minified file.
+- **Isolate**: A boolean value. If this is true, the resulting code will be isolated so it wont affect or be affected by other scripts.
+
+If the isolate option is true, the resulting code will be wrapped with the next code, assigned to a variable named as the project, so external scripts can access properly to it's content:
+
+```javascript
+var ProjectName = (function(window, undefined){
+   // Your compiled code
+})(window);
+```
+
+If you don't want your code be accesible to other scripts, just leave the project name with an empty string, resulting the wrapper as shown next:
+
+```javascript
+(function(window, undefined){
+   // Your compiled code
+})(window);
+```
+
+You can also customize the default template for new projects at `File > Compress JavaScript: Options template`. Careful! This is only for advanced users. Updates will restore the template to prevent unexpected crashes.
+
+## Special thanks
 
 Special thanks to Steffen Bruchmann and Peter Flynn, who helped me a lot on my first steps with brackets extension development.
--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 define(function (require, exports, module) {
 	'use strict';
 
-	var CommandManager  = brackets.getModule('command/CommandManager');
-	var Menus           = brackets.getModule('command/Menus');
-	var DocumentManager = brackets.getModule('document/DocumentManager');
-	var FileSystem      = brackets.getModule('filesystem/FileSystem');
-	var ExtensionUtils  = brackets.getModule('utils/ExtensionUtils');
-	var WorkspaceManager	= brackets.getModule('view/WorkspaceManager');
-	var DefaultDialogs  = brackets.getModule('widgets/DefaultDialogs');
-	var Dialogs         = brackets.getModule('widgets/Dialogs');
-	var MOZ_SourceMap	= require('SourceMap/source-map');
-	var UglifyJS        = require('UglifyJS/uglifyjs');
+	var Commands         = brackets.getModule('command/Commands');
+	var CommandManager   = brackets.getModule('command/CommandManager');
+	var Menus            = brackets.getModule('command/Menus');
+	var DocumentManager  = brackets.getModule('document/DocumentManager');
+	var FileSystem       = brackets.getModule('filesystem/FileSystem');
+	var ExtensionUtils   = brackets.getModule('utils/ExtensionUtils');
+	var WorkspaceManager = brackets.getModule('view/WorkspaceManager');
+	var DefaultDialogs   = brackets.getModule('widgets/DefaultDialogs');
+	var Dialogs          = brackets.getModule('widgets/Dialogs');
+	//var MOZ_SourceMap    = require('SourceMap/source-map');
+	var UglifyJS         = require('UglifyJS/uglifyjs');
 
 	// Log
 	function log(s) {
@@ -23,89 +24,214 @@ define(function (require, exports, module) {
 	}
 
 	// UglifyJS call
-	function doUglify() {
-		log('Executing Command Compile');
+	function doUglify(inputs, output, options) {
+		bottomPanel.show();
 		
-		// Get current file extension
-		var currentFile = DocumentManager.getCurrentDocument().file;
-		var ext = currentFile.name.split('.').pop();
-		if (ext != 'js') {
-			// Current file is not JavaScript. Warn!
-			Dialogs.showModalDialog(DefaultDialogs.DIALOG_ID_INFO, 'JS Compiler', 'Current document is not JavaScript');
+		// Get current directory
+		var directory = DocumentManager.getCurrentDocument().file.parentPath;
+		var path = directory + output;
+		
+		// Start UglifyJS magic!
+		var ast = null;
+		var compressor = UglifyJS.Compressor();
+		//var source_map = UglifyJS.SourceMap();
+		//var stream = UglifyJS.OutputStream({source_map: source_map});
+		var stream = UglifyJS.OutputStream();
+		for(var i = 0, l = inputs.length; i < l; i++){
+			appendLog('Parsing file: ' + inputs[i].name);
+			ast = UglifyJS.parse(inputs[i].content, {filename: inputs[i].name, toplevel: ast});
+		}
+		appendLog('Compressing...');
+		ast.figure_out_scope();
+		ast.transform(compressor);
+		appendLog('Mangling...');
+		ast.figure_out_scope();
+		ast.compute_char_frequency();
+		ast.mangle_names();
+		appendLog('Extracting...');
+		ast.print(stream);
+		var code = stream.toString();
+		//var map = source_map.toString();
+		
+		// Append isolation code
+		if (options.isolate) {
+			appendLog('Isolating...');
+			if (options.project) {
+				code = 'var ' + options.project + '=(function(window,undefined){' + code;
+			}
+			else{
+				code = '(function(window,undefined){' + code;
+			}
+			code += '})(window);';
+		}
+		
+		appendLog('Exporting...');
+		// Save the code
+		FileSystem.getFileForPath(path).write(code, {blind: true}, function (err) {
+			if (err) {
+				appendLog('Error compiling code at:<br/>' + path);
+				Dialogs.showModalDialog(DefaultDialogs.DIALOG_ID_INFO, 'JS Compiler', 'Error on compilation:\n' + err);
+			} else {
+				appendLog('File successfully compiled at:<br/>' + path + '<br/>Done!<br/>');
+			}
+		});
+		
+		// Save the map
+		/*FileSystem.getFileForPath(path + '.map').write(code, {blind: true}, function (err) {
+			if (err) {
+				appendLog('Error generating map at:<br/>' + path + '.map');
+				Dialogs.showModalDialog(DefaultDialogs.DIALOG_ID_INFO, 'JS Compiler', 'Error on map genetarion:\n' + err);
+			} else {
+				appendLog('Map successfully generated at:\n' + path + '.map');
+			}
+		});*/
+	}
+	
+	function getContentsFrom(options, directory, contents, counter) {
+		if (counter < options.inputs.length) {
+			// There is content pending to get. Read it!
+			appendLog('Reading ' + options.inputs[counter] + '...');
+			
+			FileSystem.getFileForPath(directory + options.inputs[counter]).read({}, function (err, content) {
+				if (err) {
+					appendLog('Error on reading this file: ' + err);
+				} else {
+					// Read content
+					contents.push({name: options.inputs[counter], content: content});
+					getContentsFrom(options, directory, contents, counter + 1);
+				}
+			});
 		} else {
-			bottomPanel.show();
-			
-			// Get current file path
-			var path = currentFile.fullPath.replace(/\.js$/, '.min.js');
-			var path_map = currentFile.fullPath.replace(/\.js$/, '.map');
-			appendLog('Compiling file:<br/>' + currentFile.fullPath);
-			
-			// Get current document text
-			var currentDocumentText = DocumentManager.getCurrentDocument().getText();
-			//log(currentDocumentText);
-
-			// Start UglifyJS magic!
-			var ast = null;
-			var compressor = UglifyJS.Compressor();
-			var source_map = UglifyJS.SourceMap();
-			var stream = UglifyJS.OutputStream();
-			var stream = UglifyJS.OutputStream({source_map: source_map});
-			appendLog('Parsing...');
-			ast = UglifyJS.parse(currentDocumentText, {filename: currentFile.fullPath, toplevel: ast});
-			appendLog('Compressing...');
-			ast.figure_out_scope();
-			ast.transform(compressor);
-			appendLog('Mangling...');
-			ast.figure_out_scope();
-			ast.compute_char_frequency();
-			ast.mangle_names();
-			appendLog('Extracting...');
-			ast.print(stream);
-			var code = stream.toString();
-			var map = source_map.toString();
-			//log(code);
-			
-			appendLog('Exporting...');
-			// Save the code
-			FileSystem.getFileForPath(path).write(code, {blind: true}, function (err) {
-				if (err) {
-					Dialogs.showModalDialog(DefaultDialogs.DIALOG_ID_INFO, 'JS Compiler', 'Error on compilation:\n' + err);
-				} else {
-					appendLog('File successfully compiled at:<br/>' + path);
-				}
-			});
-			
-			// Save the map
-			FileSystem.getFileForPath(path_map).write(code, {blind: true}, function (err) {
-				if (err) {
-					Dialogs.showModalDialog(DefaultDialogs.DIALOG_ID_INFO, 'JS Compiler', 'Error on map genetarion:\n' + err);
-				} else {
-					appendLog('Map successfully generated at:\n' + path_map + '<br/>Done!<br/>');
-				}
-			});
+			// Contents where collected
+			if (contents.length > 0) {
+				// Finally compile the code
+				doUglify(contents, options.output, options);
+			}
+			else{
+				appendLog('Something went wrong.<br/>Done!');
+			}
 		}
 	}
-
+	
+	function compileJS(){
+		log('Executing Command Compile');
+		
+		// Search for options file
+		var currentFile = DocumentManager.getCurrentDocument().file;
+		var directory = currentFile.parentPath;
+		FileSystem.getFileForPath(directory + '.jscompiler.json').read({}, function (err, content) {
+			if (err) {
+				if (err == 'NotFound') {
+					// Options file not found. Try to compile current file
+					bottomPanel.show();
+					appendLog('No options file. Compiling current script');
+					var ext = currentFile.name.split('.').pop();
+					if (ext == 'js') {
+						doUglify([{name: currentFile.name, content: DocumentManager.getCurrentDocument().getText()}], currentFile.name.replace(/\.js$/, '.min.js'));
+					} else {
+						// Current file is not JavaScript. Warn!
+						appendLog('Current document is not JavaScript');
+					}
+				} else {
+					Dialogs.showModalDialog(DefaultDialogs.DIALOG_ID_INFO, 'JS Compiler', 'Error on reading options:\n' + err);
+				}
+			} else {
+				// Open options template
+				bottomPanel.show();
+				appendLog('Loading files from options');
+				var options = JSON.parse(content);
+				getContentsFrom(options, directory, [], 0);
+			}
+		});
+	}
+	
+	function generateOptions() {
+		// Read the template
+		ExtensionUtils.loadFile(module, 'templates/.jscompiler.json').then( function(result) {
+			// Get directory and file name for project options
+			var file = DocumentManager.getCurrentDocument().file;
+			var directory = file.parentPath;
+			var directories = directory.split('/');
+			directories.pop();
+			var dirname = directories.pop();
+			var filename = file.name.replace(/\.js$/, '');
+			if (filename == file.name) {
+				filename = 'script';
+			}
+			var code = result.replace(/%DIRECTORY%/g, dirname).replace(/%FILENAME%/g, filename);
+			// Create options file
+			FileSystem.getFileForPath(directory + '.jscompiler.json').write(code, {blind: true}, function (err) {
+				if (err) {
+					Dialogs.showModalDialog(DefaultDialogs.DIALOG_ID_INFO, 'JS Compiler', 'Error on creating options:\n' + err);
+				} else {
+					// Open options file
+					CommandManager.execute(Commands.CMD_OPEN, {fullPath: directory + '.jscompiler.json'});
+				}
+			});
+		});
+	}
+	
+	function openOptions() {
+		var directory = DocumentManager.getCurrentDocument().file.parentPath;
+		var path = directory + '.jscompiler.json';
+		
+		// Reading the options file content
+		FileSystem.getFileForPath(path).read({}, function (err, content) {
+			if (err) {
+				if (err == 'NotFound') {
+					// Options file not found. Generate
+					log('Options file not found. Creating!');
+					generateOptions();
+				} else {
+					Dialogs.showModalDialog(DefaultDialogs.DIALOG_ID_INFO, 'JS Compiler', 'Error on reading options:\n' + err);
+				}
+			} else {
+				// Open options file
+				CommandManager.execute(Commands.CMD_OPEN, {fullPath: path});
+			}
+		});
+	}
+	
+	function openTemplate() {
+		var path = ExtensionUtils.getModulePath(module, 'templates/.jscompiler.json');
+		
+		// Reading the options template... Just to be sure no one has deleted it accidentally...
+		FileSystem.getFileForPath(path).read({}, function (err, content) {
+			if (err) {
+				Dialogs.showModalDialog(DefaultDialogs.DIALOG_ID_INFO, 'JS Compiler', 'Error on reading template:\n' + err);
+			} else {
+				// Open options template
+				CommandManager.execute(Commands.CMD_OPEN, {fullPath: path});
+			}
+		});
+	}
+	
 	function closePanel() {
 		bottomPanel.hide();
 	}
-
+	
 	// Add file menu option
 	var menu = Menus.getMenu(Menus.AppMenuBar.FILE_MENU);
 	menu.addMenuDivider();
-
+	
 	// Register commands
-	CommandManager.register('Compress JavaScript', 'jscompiler.compile', doUglify);
+	CommandManager.register('Compress JavaScript', 'jscompiler.compile', compileJS);
 	menu.addMenuItem('jscompiler.compile');
-
+	
+	CommandManager.register('Compress JavaScript: Options', 'jscompiler.options', openOptions);
+	menu.addMenuItem('jscompiler.options');
+	
+	CommandManager.register('Compress JavaScript: Options template', 'jscompiler.template', openTemplate);
+	menu.addMenuItem('jscompiler.template');
+	
 	// Start bottom panel
 	var bottomPanel = WorkspaceManager.createBottomPanel('jscompiler.panel', $("<div id='jscompiler-panel' class='bottom-panel vert-resizable top-resizer' style='box-sizing: border-box; height: 200px; display: block;'><div class='toolbar simple-toolbar-layout'><div class='title'>JSCompiler</div> <a href='#' class='close'>×</a></div><div id='log' class='table-container resizable-content' style='height: 170px'></div></div></div>"));
 	var panelLog = bottomPanel.$panel.find('#log');
 	bottomPanel.$panel.find('.close').on('click', closePanel);
-
+	
 	// Load css
 	ExtensionUtils.loadStyleSheet(module, 'styles/main.css');
-
+	
 	// Add toolbar icon
 	$('<a>')
 		.attr({
@@ -113,6 +239,6 @@ define(function (require, exports, module) {
 			title: 'Compress JavaScript',
 			href: '#'
 		})
-		.click(doUglify)
+		.click(compileJS)
 		.appendTo($('#main-toolbar .buttons'));
 });
